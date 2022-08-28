4a8fa4c83d6755830a8c711d9abcd3422110ced1
Danila <d.galakhin.n@gmail.com>
2022-08-28 13:37:14 +0300

    Added task

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Task 41
+
+Console.WriteLine("Task 41. Creating an array of M numbers (inputted by the user), outputting amount of numbers, which are greater than 0");
+Console.Write("Enter your numbers (divided by a space): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Amount of elements > 0: {count}");
+
