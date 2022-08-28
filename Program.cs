ee0ffa4ddcf33a250a114d816240426ab03ea2e2
Danila <d.galakhin.n@gmail.com>
2022-08-28 15:43:58 +0300

    Added tasks

--- Program.cs
+++ Program.cs
@@ -14,3 +14,18 @@ for (int i = 0; i < arr.Length; i++)
  
 Console.WriteLine($"Amount of elements > 0: {count}");
 
+// Task 43
+Console.WriteLine();
+Console.WriteLine("Aiming to find the crossing point of two lines");
+Console.WriteLine("Please, enter b1 value:");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Please, enter k1 value:");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Please, enter b2 value:");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Please, enter k2 value:");
+double k2 = double.Parse(Console.ReadLine()!);
+
+    double x = (b2 - b1)/(k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"The crossing is located at coordinates {x}, {y}");
